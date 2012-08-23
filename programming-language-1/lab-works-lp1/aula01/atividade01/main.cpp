8c06acd0872035618ce617886a50127eca61a7cb
Clauber Stipkovic <chalic@sp.r7.com>
2012-08-23 18:37:29 -0300

    Inclusão do main para a atividade 1 da aula 1

---
+++ programming-language-1/lab-works-lp1/aula01/atividade01/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+	float nota1, nota2, nota3, media;
+
+	cout << "Informa a nota 1:";
+	cin >> nota1;
+	cout << endl;
+
+	cout << "Informa a nota 2:";
+	cin >> nota2;
+	cout << endl;
+
+	cout << "Informa a nota 3:";
+	cin >> nota3;
+	cout << endl;
+
+
+	media = (nota1 + nota2 + nota3) / 3;
+
+	if (media >= 6) {
+		cout << "Média do Aulo: "
+	}
+	return 0;
+}
