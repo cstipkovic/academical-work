74824ac252699f60d1ba853fc4129a80100d4413
Clauber Stipkovic <chalic@sp.r7.com>
2012-08-29 16:04:47 -0300

    1st and 2nd lab activities implemented

--- programming-language-1/lab-works-lp1/aula01/atividade01/main.cpp
+++ programming-language-1/lab-works-lp1/aula01/atividade01/main.cpp
@@ -1,19 +1,27 @@
+/*
+	Atividade 1 - Calcula a média aritmética das 3 notas de um aluno e mostra, 
+	além do valor da média, uma mensagem de "Aprovado", caso a média seja 
+	igual ou superior a 6, ou a mensagem "reprovado", caso contrário.
+*/
+
 #include <iostream>
 
 using namespace std;
 
-int main(int argc, char const *argv[]) {
+int main() {
 	float nota1, nota2, nota3, media;
 
-	cout << "Informa a nota 1:";
+	//for (i = 0, )
+
+	cout << "Informa a nota 1: ";
 	cin >> nota1;
 	cout << endl;
 
-	cout << "Informa a nota 2:";
+	cout << "Informa a nota 2: ";
 	cin >> nota2;
 	cout << endl;
 
-	cout << "Informa a nota 3:";
+	cout << "Informa a nota 3: ";
 	cin >> nota3;
 	cout << endl;
 
@@ -21,7 +29,11 @@ int main(int argc, char const *argv[]) {
 	media = (nota1 + nota2 + nota3) / 3;
 
 	if (media >= 6) {
-		cout << "Média do Aulo: "
+		cout << "Média do(a) Aluno(a): " << media << endl;
+		cout << "Parabéns! Aprovado(a)!" << endl;
+	} else {
+		cout << "Média do(a) Aluno(a): " << media << endl;
+		cout << "Desculpe! Reprovado(a)!" << endl;
 	}
 	return 0;
 }
