e7c074d227b3da48486517009ed953f4a4e2c922
Clauber Stipkovic <clauber.halic@gmail.com>
2013-08-19 00:54:51 -0300

    Starting Lab 2 project

---
+++ programming-language-1/lab/work/Atividade Lab 2/AtividadeLab2/emploeyy.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   emploeyy.cpp
+ * Author: cstipkovic
+ * 
+ * Created on August 19, 2013, 12:54 AM
+ */
+
+#include "emploeyy.h"
+
+emploeyy::emploeyy() {
+}
+
+emploeyy::emploeyy(const emploeyy& orig) {
+}
+
+emploeyy::~emploeyy() {
+}
+
---
+++ programming-language-1/lab/work/Atividade Lab 2/AtividadeLab2/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: cstipkovic
+ *
+ * Created on August 19, 2013, 12:51 AM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
