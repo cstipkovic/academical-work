70ec02cf9032fbce9003eae4dcd87bfea40345f3
Clauber Stipkovic <clauber.halic@gmail.com>
2014-02-06 22:51:59 -0200

    java lab class 1 some examples

---
+++ programming-language-1/java/lab/aula1/Exemplo4/src/exemplo4/Exemplo4.java
@@ -0,0 +1,12 @@
+package exemplo4;
+
+public class Exemplo4 {
+
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Bem vindo");
+        sb.append(" ao Mack.");
+        String s = new String(sb);
+        System.out.println(s);
+    }
+}
