689700c0bf1dd02fa1e48fa6cb20dde6a3f28abc
Clauber Stipkovic <clauber.halic@gmail.com>
2014-02-17 20:47:16 -0300

    lab exercice

---
+++ programming-language-1/java/lab/aula1/ExercicioDiscussaoDupla/src/exerciciodiscussaodupla/ExercicioDiscussaoDupla.java
@@ -0,0 +1,13 @@
+package exerciciodiscussaodupla;
+
+public class ExercicioDiscussaoDupla {
+
+    public static void main(String[] args) {
+        int nInt = 555;
+        String a = new String();
+        
+        a = Integer.toString(nInt);
+        
+        System.out.println(a);
+    }
+}
---
+++ programming-language-1/java/lab/aula1/ExercicioDiscussaoDupla/src/exerciciodiscussaodupla/ExercicioDiscussaoDupla2.java
@@ -0,0 +1,12 @@
+package exerciciodiscussaodupla;
+
+public class ExercicioDiscussaoDupla2 {
+    
+    public static void main (String[] args) {
+        String a = null;
+                
+        a = "555";
+        
+        System.out.println(a);
+    }
+}
