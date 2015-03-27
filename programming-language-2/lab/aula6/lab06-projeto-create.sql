ab4b30ab06d563d8afe64f2ce699015667053bfe
C. Stipkovic <clauber.halic@gmail.com>
2015-03-27 20:03:47 -0300

    Update lab06-projeto-create.sql

---
+++ programming-language-2/lab/aula6/lab06-projeto-create.sql
@@ -0,0 +1,11 @@
+create table filme (
+    id_filme integer not not null,
+    titulo char(255) not null,
+    duracao char(255) not null,
+    classificacao integer not null,
+    diretor char(255) not null,
+    distribuidora char(255) not null,
+    status char(255) not null,
+    genero char(255) not null,
+    
+)
