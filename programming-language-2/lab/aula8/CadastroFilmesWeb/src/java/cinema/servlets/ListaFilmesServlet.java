a0787adc4d1b2f779d7cd5e0300facbace68d4be
Clauber Stipkovic <clauber.halic@gmail.com>
2015-04-29 12:06:20 -0300

    exercicio lp2 aula8 lab servlet listar

---
+++ programming-language-2/lab/aula8/CadastroFilmesWeb/src/java/cinema/servlets/ListaFilmesServlet.java
@@ -0,0 +1,82 @@
+package cinema.servlets;
+
+import cinema.dominio.Filme;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class ListaFilmesServlet extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        
+        HttpSession sessao = request.getSession();
+        List<Filme> filmes = (List<Filme>) sessao.getAttribute("filmes_sessao");
+        
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet ListaFilmesServlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Lista dos filmes cadastrados na sessão:</h1>");
+            out.println("<dl>");
+            for (Filme filme : filmes) {
+                out.print("<dt>" + filme.getTitulo() + "</dt>");
+                out.print("<dd>Ano: " + filme.getAno() + "</dd>");
+                out.println("</br>");
+            }
+            out.println("</dl>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
