5093b13783053174d3ee79f6fd2ebccfbf9febdb
Clauber Stipkovic <clauber.halic@gmail.com>
2016-02-21 21:12:11 -0300

    ajustando a casa pra mais um semestre

--- operating-systems/lab/atividade10/mmatriz.c
+++ operating-systems/lab/atividade10/mmatriz.c
@@ -4,75 +4,73 @@
 #include <pthread.h>
 
 void multiplica (int t, int mA[][t], int mB[][t]) {
-	printf("Function multiplica - tamanho das matrizes %ix%i\n",t,t);
-	int c, d, k, sum = 0;
-	int mAxB[t][t];
-	struct timeb tp;
-	ftime(&tp);
-	char timeString[80];
-	strftime(timeString, sizeof(timeString), "%H:%M:%S", localtime(&tp.time));
-	printf("%s:%d\n", timeString, tp.millitm);
+    printf("Function multiplica - tamanho das matrizes %ix%i\n", t, t);
+    int c, d, k, sum = 0;
+    int mAxB[t][t];
+    struct timeb tp;
+    ftime(&tp);
+    char timeString[80];
+    strftime(timeString, sizeof(timeString), "%H:%M:%S", localtime(&tp.time));
+    printf("%s:%d\n", timeString, tp.millitm);
 
-	for (c = 0; c < t; c++) {
-		for (d = 0; d < t; d++) {
-			for (k = 0; k < t; k++) {
-				sum = sum + mA[c][k]*mB[k][d];				
-			}
-		
-			mAxB[c][d] = sum;
+    for (c = 0; c < t; c++) {
+        for (d = 0; d < t; d++) {
+            for (k = 0; k < t; k++) {
+                    sum = sum + mA[c][k]*mB[k][d];				
+            }
 
-			sum = 0;
-		}
-	}
+            mAxB[c][d] = sum;
 
-	ftime(&tp);
+            sum = 0;
+        }
+    }
 
-	strftime(timeString, sizeof(timeString), "%H:%M:%S", localtime(&tp.time));
+    ftime(&tp);
 
-	printf("Fim da multiplica: %s:%d\n", timeString, tp.millitm);
+    strftime(timeString, sizeof(timeString), "%H:%M:%S", localtime(&tp.time));
+
+    printf("Fim da multiplica: %s:%d\n", timeString, tp.millitm);
 
 } // multiplica
 
 int main() {
 
-	int t, c, d, k, sum = 0;
-	int min = 0;
-	int max = 10;
+    int t, c, d, k, sum = 0;
+    int min = 0;
+    int max = 10;
 
-	struct timeb tp;
+    struct timeb tp;
 
-	// t = 543; // tamanho da matriz
+    // t = 543; // tamanho da matriz
 
-	t = 2; // tamanho da matriz
+    t = 2; // tamanho da matriz
 
-	printf("Multiplicar matrizes quadradas - o valor de t (neste caso %i) deve ser 500 + os dois dígitos do meio do seu TIA\n",t);
+    printf("Multiplicar matrizes quadradas - o valor de t (neste caso %i) deve ser 500 + os dois dígitos do meio do seu TIA\n",t);
 
-	ftime(&tp);
+    ftime(&tp);
 
-	char timeString[80];
+    char timeString[80];
 
-	strftime(timeString, sizeof(timeString), "%H:%M:%S", localtime(&tp.time));
+    strftime(timeString, sizeof(timeString), "%H:%M:%S", localtime(&tp.time));
 
-	printf("Antes de chamar a multiplica: %s:%d\n", timeString, tp.millitm);
+    printf("Antes de chamar a multiplica: %s:%d\n", timeString, tp.millitm);
 
-	int A[t][t], B[t][t], AxB[t][t];
+    int A[t][t], B[t][t], AxB[t][t];
 
-	for (c = 0; c < t; c++) {
-		for (d = 0; d < t; d++) {
-			A[c][d] = min + (rand() % (int)(max - min + 1));
-			B[c][d] = min + (rand() % (int)(max - min + 1));
-		}
+    for (c = 0; c < t; c++) {
+        for (d = 0; d < t; d++) {
+            A[c][d] = min + (rand() % (int)(max - min + 1));
+            B[c][d] = min + (rand() % (int)(max - min + 1));
+        }
+    }
 
-	}
+    multiplica(t, A, B);
 
-	multiplica(t, A, B);
+    ftime(&tp);
 
-	ftime(&tp);
+    strftime(timeString, sizeof(timeString), "%H:%M:%S", localtime(&tp.time));
 
-	strftime(timeString, sizeof(timeString), "%H:%M:%S", localtime(&tp.time));
+    printf("Fim do programa principal: %s:%d\n", timeString, tp.millitm);
 
-	printf("Fim do programa principal: %s:%d\n", timeString, tp.millitm);
-
-	return 0;
+    return 0;
 }
-
