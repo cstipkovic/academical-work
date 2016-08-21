15150d2fb75cd2dfd79ad30d5a1dbc2119432101
Clauber Stipkovic <clauber.halic@gmail.com>
2016-08-21 16:08:28 -0300

    calculadora simples em C

--- programming-language-paradigms/lab/week3/calc.c
+++ programming-language-paradigms/lab/week3/calc.c
@@ -1,16 +1,62 @@
 #include <stdio.h>
 
-int calc(int a, int b, char operator) {
-  switch (operator) {
-    case "+":
-      printf("mais\n");
-      break;
-  }
-}
-
-int main(int argc, char const *argv[]) {
+int main() {
   char operator;
+  int a, b;
 
-  scanf("%s\n", &operator);
+  printf("Escolha uma operacao aritmetica (ou digite 's' para finalizar): ");
+  scanf("%c", &operator);
+
+  while (operator != 's') {
+    switch (operator) {
+      case '+':
+        printf("\nInforme o primeiro numero: ");
+        scanf("%d", &a);
+
+        printf("\nInforme o segundo numero: ");
+        scanf("%d", &b);
+
+        printf("Resultado da soma: %d\n", (a + b));
+        break;
+
+      case '-':
+        printf("\nInforme o primeiro numero: ");
+        scanf("%d", &a);
+
+        printf("\nInforme o segundo numero: ");
+        scanf("%d", &b);
+
+        printf("Resultado da subtracao: %d\n", (a - b));
+        break;
+
+      case '/':
+        printf("\nInforme o primeiro numero: ");
+        scanf("%d", &a);
+
+        printf("\nInforme o segundo numero: ");
+        scanf("%d", &b);
+
+        if (b == 0) {
+          printf("Divisao por 0 nao permitida.\n");
+        } else {
+          printf("Resultado da divisao: %d\n", (a / b));
+        }
+        break;
+
+      case '*':
+        printf("\nInforme o primeiro numero: ");
+        scanf("%d", &a);
+
+        printf("\nInforme o segundo numero: ");
+        scanf("%d", &b);
+
+        printf("Resultado da multiplicacao: %d\n", (a * b));
+        break;
+    }
+
+    printf("\nEscolha uma operacao aritmetica (ou digite 's' para finalizar): ");
+    scanf("%c", &operator);
+  }
+
   return 0;
 }
