6b233a032ab23279a46613d3f0af1134ceab0372
Clauber Stipkovic <clauber.halic@gmail.com>
2016-08-28 15:31:27 -0300

    Provisao do java8 + tomcat7 e inclusao do app de exemplo da primeira aula

--- programming-language-3/atividade1/provision-tomcat.sh
+++ programming-language-3/atividade1/provision-tomcat.sh
@@ -1,3 +1,4 @@
+# Baseado no artigo: https://dzone.com/articles/vagrant
 # Instala o Oracle JDK via WebUpd8 PPa
 echo "Instalando Java 8..."
 sudo apt-get install -y software-properties-common python-software-properties
@@ -5,17 +6,25 @@ echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | su
 sudo add-apt-repository -y ppa:webupd8team/java
 sudo apt-get update
 sudo apt-get install -y oracle-java8-installer
+echo "Aplicando as variaveis de ambiente do Java 8..."
 sudo apt-get install -y oracle-java8-set-default
 echo "Versao do Java Instalada: "
 java -version
+
+echo "Limpando o sistema..."
 sudo apt-get autoremove -y
 
-# Apache Tomcat
+echo "Instalando Apache..."
 sudo apt-get -y install apache2
+
+echo "Instalando Tomcat..."
 sudo apt-get -y install tomcat7 tomcat7-admin
+
+echo "Instalando Maven.."
 sudo apt-get -y install maven
 
 # Configurando a variavel JAVA_HOME
+echo "Configurando o Tomcat7..."
 cd /etc/default
-sudo echo "JAVA_HOME=/usr/lib/jvm/java-7-oracle" >> tomcat7
+sudo echo "JAVA_HOME=/usr/lib/jvm/java-8-oracle" >> tomcat7
 sudo service tomcat7 restart
