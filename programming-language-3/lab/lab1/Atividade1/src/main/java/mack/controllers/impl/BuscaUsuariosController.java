86a7bc00173d32e977d8e6c54ca047cdb4cdd2f3
Clauber Stipkovic <cin_chalic@uolinc.com>
2016-09-19 18:11:40 -0300

    tentando fazer funcionar o form no servlet

--- programming-language-3/lab/lab1/Atividade1/src/main/java/mack/controllers/impl/BuscaUsuariosController.java
+++ programming-language-3/lab/lab1/Atividade1/src/main/java/mack/controllers/impl/BuscaUsuariosController.java
@@ -1,21 +1,29 @@
 package mack.controllers.impl;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import mack.controllers.AbstractController;
+import mack.entities.Usuario;
 
 public class BuscaUsuariosController extends AbstractController{
     
     public void execute() {
         try {
             this.setReturnPage("/buscaUsuarios.jsp");
-            this.getRequest().setAttribute("usuarios", this);
+            System.out.println(this.getRequest().getParameter("usuario"));
+            this.getRequest().setAttribute("usuarios", this.findUsuario());
         } catch (Exception e) {
             Logger.getLogger(BuscaUsuariosController.class.getName()).log(Level.SEVERE, null, e);
         }
     }
     
-//    public List findUsuario(String nome) {
-//        
-//    }
+    public List findUsuario() {
+        List usuarios = new ArrayList<Usuario>();
+        usuarios.add(new Usuario(1, "Mack", "Junior"));
+        usuarios.add(new Usuario(2, "Mack", "Neto"));
+        
+        return usuarios;
+    }
 }
--- programming-language-3/lab/lab1/Atividade1/src/main/webapp/buscaUsuarios.jsp
+++ programming-language-3/lab/lab1/Atividade1/src/main/webapp/buscaUsuarios.jsp
@@ -1,3 +1,5 @@
+<%@page import="mack.entities.Usuario"%>
+<%@page import="java.util.List"%>
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
@@ -6,6 +8,30 @@
         <title>JSP Page</title>
     </head>
     <body>
-        <h1>Hello World!</h1>
+        <h1>Usuarios encontrados!</h1>
+        <%
+            List<Usuario> usuarios = (List<Usuario>) request.getAttribute("usuarios");
+        %>
+        
+        <%
+            if (usuarios.size() > 0) {
+        %>
+        
+        <table>
+            <%
+                for (Usuario u:usuarios) {
+            %>
+                <tr>
+                    <td><%= u.getNome() %></td>
+                    <td><%= u.getSobrenome() %></td>
+                </tr>
+            <%
+                }
+            %>
+        </table>
+        
+        <%
+            }
+        %>
     </body>
 </html>
