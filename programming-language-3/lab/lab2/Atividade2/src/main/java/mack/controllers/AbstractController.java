08c77bbf1caf09fc134459a477cbbad83daefbcb
Clauber Stipkovic <cin_chalic@uolinc.com>
2016-09-20 17:14:48 -0300

    Atividade 2

---
+++ programming-language-3/lab/lab2/Atividade2/src/main/java/mack/controllers/AbstractController.java
@@ -0,0 +1,22 @@
+package mack.controllers;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class AbstractController {
+    
+    public void init(HttpServletRequest) {
+    }
+    
+    public void setReturnPage(String) {
+    }
+    
+    public String getReturnPage() {
+    }
+    
+    public HttpServletRequest getRequest() {
+    }
+    
+    public void setRequest(HttpServletRequest) {
+    }
+}
+ 
---
+++ programming-language-3/lab/lab2/Atividade2/src/main/java/mack/controllers/Controller.java
@@ -0,0 +1,10 @@
+package mack.controllers;
+
+import javax.servlet.http.HttpServletRequest;
+
+public interface Controller {
+    
+    public void execute();
+    public void init(HttpServletRequest request);
+    public String getReturnPage();
+}
