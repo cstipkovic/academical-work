c853f7c86dde600fb2ec0656d7b2726d0cd3d5ef
Clauber Stipkovic <clauber.halic@gmail.com>
2016-09-29 11:57:16 -0300

    trabalho de redes

--- programming-language-3/lab/lab2/Atividade2/src/main/java/mack/controllers/impl/BuscaUsuarioNomeController.java
+++ programming-language-3/lab/lab2/Atividade2/src/main/java/mack/controllers/impl/BuscaUsuarioNomeController.java
@@ -9,15 +9,19 @@ import mack.dao.usuario.UsuarioDAO;
 import mack.dao.usuario.UsuarioDAOFactory;
 import mack.entities.Usuario;
 
-public class BuscaController extends AbstractController {
+public class BuscaUsuarioNomeController extends AbstractController {
 
     public void execute() {
         try {
             List usuarios = new ArrayList<Usuario>();
+            this.getRequest().getParameter("usuario");
+            System.out.println("teste");
             UsuarioDAO dao = UsuarioDAOFactory.getUsuarioDAO();
 //            usuarios = (List) dao.buscaUsuarioPorNome()
+            this.setReturnPage("/Lista.jsp");
+            this.getRequest().setAttribute("usuarios", usuarios);
         } catch (Exception e) {
-            Logger.getLogger(BuscaController.class.getName()).log(Level.SEVERE, null, e);
+            Logger.getLogger(BuscaUsuarioNomeController.class.getName()).log(Level.SEVERE, null, e);
         }
     }
     
