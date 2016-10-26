a01451b904a96832e2b2ade18397f4d0a9c33370
Clauber Stipkovic <clauber.halic@gmail.com>
2016-10-25 23:56:45 -0200

    Atividade 4 em andamento

---
+++ programming-language-paradigms/lab/prolog-lista2/README.md
@@ -0,0 +1,5 @@
+# Prolog - Lista 2
+
+Alunos:
+- Ricardo Arthur de Souza Simião - 31676987
+- Clauber Pereira Stipkovic Halic  - 31243045
--- programming-language-paradigms/lab/prolog-lista2/atividade3.pl
+++ programming-language-paradigms/lab/prolog-lista2/atividade3.pl
@@ -1,4 +1,8 @@
 % 3. Contar o número de vezes que um certo elemento aparece em uma dada lista.
 count([], _, 0).
+count([H|T], H, X) :-
+  count(T, H, C),
+  X is C + 1.
 count([H|T], N, X) :-
-    H is X + 1.
+  count(T, N, X),
+  H\=N.
