f73c03b977a07b85b1e4bdba1153758029370a52
C. Stipkovic <clauber.halic@gmail.com>
2018-03-04 16:23:53 -0300

    ignore, ex2 and 3

--- parallel-computing/lab/recordando-parte1/ex2/ex2.c
+++ parallel-computing/lab/recordando-parte1/ex2/ex2.c
@@ -1,11 +1,29 @@
 #include <stdio.h>
+
 int main() {
-    float n1, n2, n3, me;
+    float n1, n2, n3, me, ma;
 
-    printf("Informe as notas nesse formato '2.3, 3.4, 9.3': ");
-    scanf("%f, %f, %f", &n1, &n2, &n3);
+    printf("Informe 'n1, n2, n3, me': ");
+    scanf("%f, %f, %f, %f", &n1, &n2, &n3, &me);
 
-    printf("%f, %f, %f", n1, n2, n3);
+    ma = (n1 + (n2 * 2) + (n3 * 3) + me) / 7;
+        
+    if (ma >= 9.0)
+    {
+        printf("Conteito A para media %.2f\n", ma);
+    } else if (ma >= 7.5)
+    {
+        printf("Conceito B para media %.2f\n", ma);
+    } else if (ma >= 6.0)
+    {
+        printf("Conceito C para media %.2f\n", ma);
+    } else if (ma >= 4.0)
+    {
+        printf("Conceito D para media %.2f\n", ma);
+    } else
+    {
+        printf("Conceito E para media %.2f\n", ma);
+    }
 
     return 0;
 }
