45443b4becd732158786a7a8dfeaf19208ff3752
C. Stipkovic <clauber.halic@gmail.com>
2018-03-06 11:27:36 -0300

    Ex3 lab

--- parallel-computing/lab/recordando-parte1/ex3/ex3.c
+++ parallel-computing/lab/recordando-parte1/ex3/ex3.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 
 int main() {
+    int a[9];
+    int i;
+
+    for (i = 0; i < 9; i++)
+    {
+        a[i] = i + 1;
+    }
+
+    for (i = 0; i < 9; i++)
+    {
+        printf("%d", a[i]);
+    }
+
+    printf("\n");
+
     return 0;
 }
---
+++ parallel-computing/lab/recordando-parte1/ex4/ex4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char nomes[1][2];
+    int i, j;
+
+    nomes[0][0] = 'f';
+    nomes[0][1] = 'e';
+
+    for (i = 0; i < 1; i++)
+    {
+        for (j = 0; j < 2; i++)
+        {
+            printf("%c", nomes[i][j]);
+        }   
+    }
+}
