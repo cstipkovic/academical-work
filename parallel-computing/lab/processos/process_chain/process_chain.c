ca520a23c75a001ee6aa17606267d3143cfdc611
C. Stipkovic <clauber.halic@gmail.com>
2018-03-13 12:10:31 -0300

    Resolving the lab process_chain exercise

--- parallel-computing/lab/processos/process_chain/process_chain.c
+++ parallel-computing/lab/processos/process_chain/process_chain.c
@@ -19,7 +19,10 @@ int main(int argc, char *argv[])
         if (childpid = fork())
             break;
 
-    fprintf(stderr, "i:%d process ID:%ld parent ID:%ld child ID:%ld\n",
+    // fprintf(stderr, "i:%d process ID:%ld parent ID:%ld child ID:%ld\n",
+    //         i, (long)getpid(), (long)getppid(), (long)childpid);
+
+    fprintf(stdout, "i:%d process ID:%ld parent ID:%ld child ID:%ld\n",
             i, (long)getpid(), (long)getppid(), (long)childpid);
 
     return 0;
@@ -27,8 +30,16 @@ int main(int argc, char *argv[])
 
 /*
 1-  Rode o programa anterior para valores grandes de n. 
-    As mensagens sempre estar˜ao ordenadas pelo valor de i? 
+    As mensagens sempre estarao ordenadas pelo valor de i? 
 
-2-  O que acontece se o programa anterior escreve - se as mensagens para sys.stdout,
-    usando print, ao inv´es de para sys.stderr ?
+    Resposta: Não, pois o tempo de mudança de estado de um processo para “feito” varia, 
+        dependendo da fila de prioridades dos processos que estão em estado de “execução” 
+        naquele momento.
+
+2-  O que acontece se o programa anterior escreve-se as mensagens para sys.stdout,
+    usando print, ao inv´es de para sys.stderr?
+
+    Resposta: Nao consegui verificar ao certo a diferenca, mas pelo que notei o stdout
+        faz a saida um pouco mais rapida do que o stderr, ja que ele nao apresenta uma 
+        descricao mais detalhada de um erro, como e o caso do stderr.
 */
