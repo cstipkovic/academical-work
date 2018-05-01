38ed212e295462635e231fbad6428e70b612f29d
Clauber Stipkovic <clauber@intvbrasil.com.br>
2018-04-30 22:39:04 -0300

    Removing test socket using node

--- distributed-computing/lab/clock-sync/master.js
+++
@@ -1,8 +0,0 @@
-const master = require('./src/Master');
-
-const masterServer = new master(41234, 'localhost');
-
-const date = Date.now();
-const message = date.toString();
-
-masterServer.send(message, 47777, 'localhost');
